98657da37083a955e5bcb1370e45f0037882afda
TranDucVuong2003 <tranvuong310103@gmail.com>
2023-10-01 15:34:10 +0700

    Tạo Object

--- Net2/Program.cs
+++ Net2/Program.cs
@@ -1,11 +1,10 @@
-﻿using System.Security.AccessControl;
-using Net2.Models;
-int n = Convert.ToInt16(co)
-int []intArray = new int[];
-for (int i = 0; i < ; i++)
+﻿using Net2.Models;
+public class Program 
 {
-    System.Console.WriteLine("Nhap phan tu thu {0}:", i);
-    object intArray = null;
-    intArray[i] = a;
+    private static void Main(string[] args)
+    {
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+
+    }
 }
-0395650957
