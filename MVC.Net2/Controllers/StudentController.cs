78240384b4776ab4decb4917bedd8f07e35ba935
TranDucVuong2003 <tranvuong310103@gmail.com>
2023-10-07 14:37:48 +0700

    C# PT Bac 2

--- MVC.Net2/Controllers/StudentController.cs
+++ MVC.Net2/Controllers/StudentController.cs
@@ -1,9 +1,36 @@
+using System.Xml;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MVC.Net2.Controllers;
-public class Student : Controller
+public class StudentController : Controller
 {
-    public IActionResult Money(){
+    public IActionResult Money()
+    {
         return View();
     }
+    [HttpPost]
+    public IActionResult Money(int Number1, int Number2, int Number3)
+    {
+        float Delta = (Number2 * Number2) - 4 * Number1 * Number3;
+        string strOutput = "";
+        if (Delta > 0)
+        {
+            double x1 = (-Number2 + Math.Sqrt(Delta)) / (2 * Number1);
+            double x2 = (-Number2 - Math.Sqrt(Delta)) / (2 * Number1);
+            strOutput = "X1 = " + x1 + "X2 = " + x2;
+        }
+        else if (Delta == 0)
+        {
+            double x = -Number2 / (2 * Number1);
+            strOutput = "Phương trình có nghiệm kép:" + x;
+        }
+        else
+        {
+            strOutput = "Phương trình không có nghiệm thực";
+
+        }
+        ViewBag.Ketqua = strOutput;
+        return View();
+    }
+
 }
